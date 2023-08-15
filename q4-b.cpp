bade2144749eb4af68de844461a29c859a9c3fd9
Tanya-Sood <116866471+Tanya-Sood@users.noreply.github.com>
2023-08-15 15:29:38 +0530

    Add files via upload

--- q4-b.cpp
+++ q4-b.cpp
@@ -2,5 +2,28 @@
 using namespace std;
 int main() 
 {
-   
+    int arr1[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    int arr2[3][3]={{1,2,3},{3,4,5},{5,6,7}};
+    int i,j,k,arr[3][3];
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            arr[i][j]=0;
+            for(k=0;k<3;k++)
+            {
+                arr[i][j]+=arr1[i][k]*arr2[k][j];
+            }
+        }
+    }
+    cout<<"Multiplication of two matrix is : "<<endl;
+    for (i = 0; i < 3 ; i++)
+    {
+        for ( j = 0; j < 3 ; j++)
+        {
+            cout<<"\t"<<arr[i][j];
+        }
+        cout<<endl;
+    }
 }
